ecd70e8c43afbf178b9d258c7976a46e7bec48bc
kingflamez <flamekeed@gmail.com>
2021-04-22 22:44:08 +0100

    update url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://stormy-mesa-05417.herokuapp.com/graphql',
+  uri: 'https://wild-slides.herokuapp.com/graphql',
   cache: new InMemoryCache()
 });
 
